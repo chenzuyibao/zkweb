68cf3de2e1664582b2184d81936b8c6b6fcb8c51
chenzuyibao <chenzuyibao@163.com>
2019-04-06 22:46:42 +0800

    改为jar部署

--- src/main/java/com/yasenagat/zkweb/ZkWebSpringBootApplication.java
+++ src/main/java/com/yasenagat/zkweb/ZkWebSpringBootApplication.java
@@ -15,37 +15,19 @@ import org.springframework.context.annotation.ComponentScan;
  * @author Shandy
  *
  */
-//@SpringBootApplication(exclude = {WebFluxAutoConfiguration.class,ReactorCoreAutoConfiguration.class})
 @SpringBootApplication
 @ComponentScan
 @ServletComponentScan
-//SpringBootServletInitializer for war打包方式
-public class ZkWebSpringBootApplication extends SpringBootServletInitializer{
-	private final static Logger logger = LoggerFactory.getLogger(ZkWebSpringBootApplication.class);
-	public final static String applicationYamlFileName="application-zkweb.yaml";
-//	@Bean(name = "sessionProperties")
-//    @Qualifier(value = "sessionProperties")
-//    public SessionProperties sessionProperties(){
-//        return new SessionProperties(null);
-//    }
-//	@Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(SpringBootApplication.class);
-//    }
-	//for war打包方式
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		String file=ZkWebSpringBootApplication.class.getClassLoader().getResource(applicationYamlFileName).getFile();
-    	logger.info("applicationYamlFileName({})={}",applicationYamlFileName,file);
-		application.properties("spring.config.location=classpath:/"+applicationYamlFileName);
-        return application.sources(ZkWebSpringBootApplication.class);
-    }
+public class ZkWebSpringBootApplication extends SpringBootServletInitializer {
+    private final static Logger logger                  = LoggerFactory
+        .getLogger(ZkWebSpringBootApplication.class);
+    public final static String  applicationYamlFileName = "application-zkweb.yaml";
+    
     public static void main(String[] args) {
-    	//System.setProperty("spring.config.location", "classpath*:/application-zkweb.yaml");
-    	//SpringApplication.run(ZkWebSpringBootApplication.class, args);
-    	String file=ZkWebSpringBootApplication.class.getClassLoader().getResource(applicationYamlFileName).getFile();
-    	logger.info("applicationYamlFileName({})={}",applicationYamlFileName,file);
-    	new SpringApplicationBuilder(ZkWebSpringBootApplication.class).
-			properties("spring.config.location=classpath:/"+applicationYamlFileName).run(args);
+        String file = ZkWebSpringBootApplication.class.getClassLoader()
+            .getResource(applicationYamlFileName).getFile();
+        logger.info("applicationYamlFileName({})={}", applicationYamlFileName, file);
+        new SpringApplicationBuilder(ZkWebSpringBootApplication.class)
+            .properties("spring.config.location=classpath:/" + applicationYamlFileName).run(args);
     }
 }
